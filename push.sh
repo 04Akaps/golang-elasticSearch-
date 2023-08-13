fb6325cfff2c1b8ada6a9c3a996a9d44015175c6
유호진 <hojin.yu@supercat.co.kr>
2023-08-13 13:14:09 +0900

    sh test

--- push.sh
+++ push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-branch="main"
+remote="me"
+branch="master"
 
 read -p "커밋 메시지를 입력하세요: " commit_message
 
@@ -10,6 +11,11 @@ git add .
 echo commit File
 git commit -m "$commit_message"
 
+echo push All File to $branch branch
 
-echo push All File
-git push me $branch
+if git ls-remote --exit-code --heads $remote $branch; then
+    git push $remote $branch
+    echo "Push successful!"
+else
+    echo "Error: Remote branch $branch does not exist."
+fi
